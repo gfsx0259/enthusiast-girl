b43f5c78ddf2549a3e89ca85e9771edea8380f74
k.popov <k.popov@it.ecommpay.com>
2023-11-27 22:23:57 +0300

    Trigger build command using branch job with tag

--- app/command/build/BuildCommand.go
+++ app/command/build/BuildCommand.go
@@ -5,13 +5,12 @@ import (
 	"deployRunner/config"
 	"errors"
 	"fmt"
-	"log"
 	"strings"
 )
 
 const (
 	GetCrumbCommand   string = "curl -u %s:%s -s \"https://ci.platformtests.net/crumbIssuer/api/xml\" | xmllint --format --xpath \"concat(//crumbRequestField,':',//crumb)\" -"
-	TriggerJobCommand string = "curl -I -X POST https://%s:%s@ci.platformtests.net/job/ecommpay/job/pp/job/concept-%s/job/master/buildWithParameters -d \"FORCE_TAG=%s\" -H \"%s\""
+	TriggerJobCommand string = "curl -X POST https://%s:%s@ci.platformtests.net/job/ecommpay/job/pp/job/concept-%s/job/master/buildWithParameters -d \"FORCE_TAG=%s\" -H \"%s\""
 )
 
 type Command struct {
@@ -29,14 +28,12 @@ func New(application string, tag string, sdlc *config.Sdlc) Command {
 func (c Command) Run() (string, error) {
 	crumb, err := command.Execute(fmt.Sprintf(GetCrumbCommand, c.sdlc.User, c.sdlc.Password), "")
 	if err != nil {
-		log.Fatalf("Can not get jenknns crumb: %s", err)
-		return "", err
+		return "", errors.New(fmt.Sprintf("error occurred while fetching jenknns crumb: %s", err.Error()))
 	}
 
 	response, err := command.Execute(fmt.Sprintf(TriggerJobCommand, c.sdlc.User, c.sdlc.Token, c.params.Application, c.params.Tag, crumb), "")
 	if err != nil {
-		log.Fatalf("Can not trigger jenknns job: %s", err)
-		return "", err
+		return "", errors.New(fmt.Sprintf("error occurred while triggering jenkins job: %s", err.Error()))
 	}
 
 	if strings.Contains(response, "404") {
